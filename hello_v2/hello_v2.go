f79cc0be652f208504ae0da6331138d26b631343
invalidred <5270458+invalidred@users.noreply.github.com>
2023-10-04 22:30:58 -0400

    inital commit

---
+++ hello_v2/hello_v2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Abdul")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(message)
+
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	messages, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(messages)
+}
